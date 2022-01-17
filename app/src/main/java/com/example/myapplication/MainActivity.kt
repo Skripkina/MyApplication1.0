8a955b477b51b8ce07cc89daa5b94d8a219f6691
Скрипкина Ю.А <ua.skripkina@tensor.ru>
2022-01-17 20:04:34 +0300

    my first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.myapplication.databinding.StartPageBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: StartPageBinding
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext,"CORRECT!",Toast.LENGTH_SHORT).show()
             }
         }
+        //init
 
     }
 
