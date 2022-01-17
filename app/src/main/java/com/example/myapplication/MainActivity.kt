721fb7f74feb2deb3e59fc3e22d67af259dfb573
Скрипкина Ю.А <ua.skripkina@tensor.ru>
2022-01-17 20:03:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.StartPageBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: StartPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = StartPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            button.setOnClickListener {
+                Toast.makeText(applicationContext,"CORRECT!",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+}
