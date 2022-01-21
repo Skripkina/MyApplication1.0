e7ba28b23d4f0f95f09cd1a208a5faf7d5e17c83
Скрипкина Ю.А <ua.skripkina@tensor.ru>
2022-01-21 20:18:41 +0300

    add click button start game, add SovaActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.myapplication.databinding.StartPageBinding
 import kotlin.math.log
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: StartPageBinding
@@ -13,13 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = StartPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        with(binding){
-            button.setOnClickListener {
-                Toast.makeText(applicationContext,"CORRECT!",Toast.LENGTH_SHORT).show()
-            }
-        }
-        //init
+//        with(binding){
+//            button.setOnClickListener {
+//                Toast.makeText(applicationContext,"CORRECT!",Toast.LENGTH_SHORT).show()
+//            }
+//        }
+    }
 
+    fun onClickStartGame() {
+        Intent(this, SovaActivity::class.java)
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/SovaActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.example.myapplication.databinding.StartPageBinding
+import android.app.Activity
+
+class SovaActivity : Activity(){
+
+    private lateinit var binding: StartPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.round_sova)
+
+    }
+
+}
